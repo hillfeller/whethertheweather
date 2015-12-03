17bb6f827915b7f68421a70dfe789946eccc33d2
Hillary <hillfeller@gmail.com>
2015-12-03 16:02:25 -0500

    Posts CRUD

---
+++ whethertheweather/app/controllers/users_controller.rb
@@ -0,0 +1,11 @@
+class UsersController < ApplicationController
+  # before_action :authenticate_user!
+
+  def show
+    @user = User.find(params[:id]) if params[:id].present?
+    unless @user
+      @user = current_user
+    end
+  end
+
+end

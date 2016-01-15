8854233a4a6cc2fc19315be2835be9a9b6ff328f
Hillary <hillfeller@gmail.com>
2016-01-14 21:28:44 -0400

    Removed Posts from Likes

--- whethertheweather/db/schema.rb
+++ whethertheweather/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160115005128) do
+ActiveRecord::Schema.define(version: 20160115012223) do
 
   create_table "ailments", force: :cascade do |t|
     t.text     "description"

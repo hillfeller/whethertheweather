3349c0f94a4e69af44e0103d47e35b064004dc1a
Hillary <hillfeller@gmail.com>
2016-01-24 14:37:18 -0500

    Removed two ailments migration files

--- whethertheweather/db/migrate/20160115004807_remove_user_from_ailments.rb
+++
@@ -1,6 +0,0 @@
-class RemoveUserFromAilments < ActiveRecord::Migration
-  def change
-    remove_column :ailments, :integer
-    remove_index :ailments
-  end
-end
--- whethertheweather/db/migrate/20160115005128_add_user_to_ailments.rb
+++
@@ -1,6 +0,0 @@
-class AddUserToAilments < ActiveRecord::Migration
-  def change
-    add_column :ailments, :user_id, :integer, index: true, foreign_key: true
-
-  end
-end

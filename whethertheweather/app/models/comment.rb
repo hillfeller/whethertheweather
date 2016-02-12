ab945207c35a53f99c486ee4a07093d61664661e
Hillary <hillfeller@gmail.com>
2016-02-11 19:55:35 -0500

    Removal of comment validation

--- whethertheweather/app/models/comment.rb
+++ whethertheweather/app/models/comment.rb
@@ -5,7 +5,6 @@ class Comment < ActiveRecord::Base
   has_many :labelings, as: :labelable
   has_many :labels, through: :labelings
   default_scope { order('updated_at DESC') }
-  validates :body, length: {maximum: 150}, presence: true
 
 
 end
